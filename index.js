8d6fd210b838297562cb709cc58996e0fbe0dbfb
ryo-yamamo10 <r.yamamo10@gmail.com>
2021-05-01 17:11:01 +0900

    update app

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 'use strict';
 const http = require('http');
+const auth = require('http-auth');
 const router = require('./lib/router');
-const server = http.createServer((req,res) => {
+const basic = auth.basic({
+  realm: 'Enter username and password', 
+  file: './users.htpasswd'
+});
+
+const server = http.createServer(basic, (req,res) => {
   //res.end('hi');
   router.route(req,res);
 }).on('error', e => {
@@ -14,3 +20,4 @@ const port = 8000;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
+
